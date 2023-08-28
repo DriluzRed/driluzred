d4bf9ab227684e10e2fcd755283d9e66fe576a9a
DriluzRed <arturorafa74@gmail.com>
2023-08-27 22:50:56 -0400

    se agrega tailwindcss

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <>
+            <div className="container mx-auto p-4">
+                <h1 className="text-2xl font-bold">¡Hola, Tailwind CSS en Vite!</h1>
+                <button className="bg-red-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-4 rounded">
+                    Botón de Tailwind
+                </button>
+            </div>
+        </>
+    )
+}
