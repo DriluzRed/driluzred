25ac81e09f58e189f49fe7e21b391541a34d57ef
DriluzRed <arturorafa74@gmail.com>
2023-08-27 22:43:51 -0400

    cambio de readme

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">¡Hola, Tailwind CSS en Vite!</h1>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-4 rounded">
+        Botón de Tailwind
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
